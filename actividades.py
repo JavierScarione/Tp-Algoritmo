132ae1da11a0993bbe1d488d42e8e1aba323c399
JavierScarione <javier.scarione7@gmail.com>
2024-04-21 18:06:59 -0300

    On branch Javier-Scarione_Algoritmo Your branch is up to date with 'origin/Javier-Scarione_Algoritmo'. Changes to be committed: new file:   Actividades_5y22.py modified:   actividades.py

--- actividades.py
+++ actividades.py
@@ -8,7 +8,7 @@ def fibonacci(num):
         return fibonacci(num-1) + fibonacci(num-2)
 
 
-# print(fibonacci(10))
+#print(fibonacci(10))
 
 
 # actividad 2
@@ -19,7 +19,7 @@ def sumatoria(num):
         return num + sumatoria(num-1)
 
 
-# print(sumatoria(10))
+#print(sumatoria(10))
 
 
 # actividad 3
@@ -30,7 +30,7 @@ def producto(num, num2):
         return num + producto(num, num2-1)
 
 
-# print(producto(2, 5))
+#print(producto(2, 5))
 
 
 # actividad 4
@@ -41,7 +41,7 @@ def potencia(base, exponente):
         return base * potencia(base, exponente-1)
 
 
-# print(potencia(2, 5))
+#print(potencia(2, 5))
 
 
 # actividad 6
@@ -52,7 +52,7 @@ def invertir_palabra(palabra):
         return palabra[-1] + invertir_palabra(palabra[0:-1])
 
 
-# print(invertir_palabra("hola"))
+#print(invertir_palabra("hola"))
 
 
 # actividad 7
@@ -63,7 +63,7 @@ def fraccion(denominador):
         return 1 / denominador + fraccion(denominador-1)
 
 
-# print(fraccion(6))
+#print(fraccion(6))
 
 
 # actividad 8
@@ -74,7 +74,7 @@ def convertir_binario(num):
         return convertir_binario(num // 2) + str(num % 2)
 
 
-# print(convertir_binario(8))
+#print(convertir_binario(8))
 
 
 # actividad 10
@@ -85,7 +85,7 @@ def contar_digitos(num):
         return 1 + contar_digitos(num // 10)
 
 
-# print(contar_digitos(5467))
+#print(contar_digitos(5467))
 
 
 # actividad 11
@@ -96,7 +96,7 @@ def invertir_numero(num):
         return (num % 10) * 10 ** len(str(num // 10)) + invertir_numero(num // 10)
 
 
-# print(invertir_numero(1432))
+#print(invertir_numero(1432))
 
 
 # actividad 14
@@ -106,8 +106,24 @@ def sumar_digitos(num):
     else:
         return (num % 10) + sumar_digitos(num // 10)
 
+#print(sumar_digitos(1223))
 
-# print(sumar_digitos(1223))
+
+# actividad 15
+def raiz_cuadrada(num, valor=1):
+    if num == 0 or num == 1:
+        return num
+    else:
+        resultado = valor * valor
+        if resultado == num:
+            return valor
+        elif resultado > num:
+            return valor - 1
+        else:
+            return raiz_cuadrada(num, valor+1)
+
+
+#print(raiz_cuadrada(16))
 
 
 # actividad 17
@@ -119,4 +135,4 @@ def reordenar_lista(lista):
         reordenar_lista(lista[0:-1])
 
 
-# print(reordenar_lista(lista=["juan", "jorge", "maria", "pepe"]))
+#print(reordenar_lista(lista=["juan", "jorge", "maria", "pepe"]))
